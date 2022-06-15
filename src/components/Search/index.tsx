eec742070456a4d3636b32b43226f2e746ac0434
João Gabriel <main.joaogabrielsg@gmail.com>
2022-06-14 22:24:33 -0300

    feat: filter countries by search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,16 +1,36 @@
 import { MagnifyingGlass } from 'phosphor-react';
+import { FormEvent, useEffect, useState } from 'react';
+
+import { useDebouncedValue } from '@/shared/hooks';
 
 import * as S from './styles';
 
-export function Search() {
+interface IPropsSearch {
+  onSearchCountry: (country: string) => void;
+}
+
+export function Search({ onSearchCountry }: IPropsSearch) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedSearchTerm = useDebouncedValue(searchTerm.trim());
+
+  function handleSearch(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
+
+  useEffect(() => {
+    onSearchCountry(debouncedSearchTerm);
+  }, [onSearchCountry, debouncedSearchTerm]);
+
   return (
-    <S.Container>
+    <S.Container onSubmit={handleSearch}>
       <S.Field htmlFor="search">
         <MagnifyingGlass size={24} />
         <S.Input
           type="text"
           id="search"
           placeholder="Search for a country..."
+          value={searchTerm}
+          onChange={({ target }) => setSearchTerm(target.value)}
         />
       </S.Field>
     </S.Container>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import { useState } from 'react';
 
 import { Countries, FilterByRegion, Search } from '@/components';
 import { CountriesService } from '@/shared/services';
@@ -6,18 +7,35 @@ import { TCountry } from '@/shared/types';
 import * as S from '@/styles/pages/Home';
 
 interface IPropsHome {
-  countries: TCountry[] | null;
+  countries: TCountry[];
 }
 
 export default function Home({ countries }: IPropsHome) {
+  const [filteredCountries, setFilteredCountries] = useState(countries);
+
+  function onSearchCountry(countryToSearch: string) {
+    if (countryToSearch === '') {
+      setFilteredCountries(countries);
+      return;
+    }
+
+    const newFilteredCountries = countries
+      ? countries.filter((country) =>
+          country.name.common.toLowerCase().includes(countryToSearch),
+        )
+      : [];
+
+    setFilteredCountries(newFilteredCountries);
+  }
+
   return (
     <S.Container>
       <S.FiltersSection>
-        <Search />
+        <Search onSearchCountry={onSearchCountry} />
         <FilterByRegion />
       </S.FiltersSection>
 
-      {!!countries && <Countries countries={countries} />}
+      <Countries countries={filteredCountries} />
     </S.Container>
   );
 }
@@ -27,7 +45,7 @@ export const getStaticProps: GetStaticProps<IPropsHome> = async () => {
 
   return {
     props: {
-      countries,
+      countries: countries || [],
     },
     revalidate: 60 * 60 * 24, // 1 day
   };
